e35a05ace62363e1abe81c4ec8098c7b984d29e7
Bobae Kang <bkang.kr@gmail.com>
2019-09-19 13:49:17 -0500

    fix: use award statement for websites

--- src/components/FundingAcknowledgement.vue
+++ src/components/FundingAcknowledgement.vue
@@ -5,12 +5,12 @@
     style="max-width: 960px;"
   >
     <template>{{
-      "This product was supported by State Justice Statistics (SJS) \
-Program Grant #2018-86-CX-K006, awarded to the Illinois Criminal \
-Justice Information Authority by the U.S. Department of Justice \
-Office of Justice Programs' Bureau of Justice Statistics. \
-Points of view in this product do not necessarily represent the \
-official position or policies of the U.S. Department of Justice."
+      'This Web site is funded through a grant from the Bureau of Justice \
+Statistics, Office of Justice Programs, U.S. Department of Justice. \
+Neither the U.S. Department of Justice nor any of its components operate, \
+control, are responsible for, or necessarily endorse, this Web site \
+(including, without limitation, its content, technical infrastructure, \
+and policies, and any services or tools provided).'
     }}</template>
   </div>
 </template>
