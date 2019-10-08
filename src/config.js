7c6590876ebde2d5aaebd4a36ac3cecc7318ae1e
Bobae Kang <bkang.kr@gmail.com>
2019-10-08 11:54:56 -0500

    refactor: unnest config github info

--- src/config.js
+++ src/config.js
@@ -6,14 +6,12 @@ module.exports = {
   apiBaseURL:
     process.env.VUE_APP_API_BASE_URL || 'https://research.icjia-api.cloud',
   gaAPIKey: process.env.VUE_APP_GA_ID || '',
-  github: {
-    url: 'https://github.com/icjia/icjia-research',
-    version: '1.0.0-beta.9'
-  },
+  github: 'https://github.com/icjia/icjia-research',
   meta: {
     title: 'Research Hub',
     description:
       'Welcome to the Illinois Crinimal Justice Information Authority (ICJIA) Research Hub. Here you can find various criminal justice datasets, research publications and resources to develop web applications, dashboards and more.'
   },
-  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/researchhub/'
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/researchhub/',
+  version: '1.0.0-beta.9'
 }
