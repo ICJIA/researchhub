ca71cf0d70c011ef93693504cbc0f9310d645c3c
Bobae Kang <bkang.dev@gmail.com>
2019-10-08 10:13:10 -0500

    refactor: move footer info to config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  agency: {
+    name: 'Illinois Criminal Justice Information Authority',
+    url: 'http://www.icjia.state.il.us/'
+  },
   apiBaseURL:
     process.env.VUE_APP_API_BASE_URL || 'https://research.icjia-api.cloud',
   gaAPIKey: process.env.VUE_APP_GA_ID || '',
+  github: {
+    url: 'https://github.com/icjia/icjia-research',
+    version: '1.0.0-beta.9'
+  },
   publicPath: process.env.VUE_APP_PUBLIC_PATH || '/researchhub/'
 }
