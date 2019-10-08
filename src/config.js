9a0906d0bf758c8b4ac1eacfa5b5e9f809caf567
Bobae Kang <bkang.dev@gmail.com>
2019-10-08 10:16:36 -0500

    refactor: move meta info to config

--- src/config.js
+++ src/config.js
@@ -10,5 +10,10 @@ module.exports = {
     url: 'https://github.com/icjia/icjia-research',
     version: '1.0.0-beta.9'
   },
+  meta: {
+    title: 'Research Hub',
+    description:
+      'Welcome to the Illinois Crinimal Justice Information Authority (ICJIA) Research Hub. Here you can find various criminal justice datasets, research publications and resources to develop web applications, dashboards and more.'
+  },
   publicPath: process.env.VUE_APP_PUBLIC_PATH || '/researchhub/'
 }
