d99857dfb2f6f947585adb3b392b21110e36003a
Bobae Kang <bkang.kr@gmail.com>
2019-10-09 09:18:49 -0500

    chore(config): remove irrelevant items

--- src/config.js
+++ src/config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  agency: {
-    name: 'Illinois Criminal Justice Information Authority',
-    url: 'http://www.icjia.state.il.us/'
-  },
   apiBaseURL:
     process.env.VUE_APP_API_BASE_URL || 'https://research.icjia-api.cloud',
   gaAPIKey: process.env.VUE_APP_GA_ID || '',
@@ -12,6 +8,5 @@ module.exports = {
     description:
       'Welcome to the Illinois Crinimal Justice Information Authority (ICJIA) Research Hub. Here you can find various criminal justice datasets, research publications and resources to develop web applications, dashboards and more.'
   },
-  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/researchhub/',
-  version: '1.0.0-beta.9'
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/researchhub/'
 }
