9e749d038c49da93d6af170cd0410cc6201689a5
Bobae Kang <bkang.dev@gmail.com>
2019-10-11 16:32:33 -0500

    chore(config): update default values as necessary

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apiBaseURL:
-    process.env.VUE_APP_API_BASE_URL || 'https://research.icjia-api.cloud',
+    process.env.VUE_APP_API_BASE_URL || 'https://researchhub.icjia-api.cloud',
   gaAPIKey: process.env.VUE_APP_GA_ID || '',
   github: 'https://github.com/icjia/icjia-research',
   meta: {
